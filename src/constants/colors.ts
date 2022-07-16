9831cff5f1ea6d2540c0f8d8692eda6e97c97fd1
Andrey Rukavitsyn <rkvtsn@gmail.com>
2022-07-16 06:13:39 -0400

    [editor] - feature added outer click and colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,42 @@
+export enum COLOR_PALETTE {
+  YellowField = "#F5CF8E",
+  YellowTerra = "#DC7651",
+  YellowCamel = "#bc9e6e",
+
+  RedJelly = "#E4684D",
+  RedRose = "#C62460",
+
+  BlueBean = "#4B8195",
+  BlueNavy = "#1271C2",
+
+  BrownSpicy = "#88534F",
+  BrownCoffee = "#6C4C36",
+  BrownBean = "#443224",
+  BrownChocolate = "#7B5941",
+  BrownRusset = "#864224",
+  BrownAuborn = "#972E22",
+
+  GrayGranit = "#646468",
+  GrayCadet = "#aab1bc",
+
+  GreenSheen = "#6AB0A5",
+  GreenLime = "#97C90E",
+  GreenOcean = "#59BC8C",
+  GreenAndroid = "#9fc146",
+  GreenPistachio = "#A2D172",
+
+  VioletBlue = "#59409B",
+
+  BlackSpace = "#49494C",
+  BlackTree = "#2E2B22",
+  BlackBlue = "#271f3f",
+  BlackChinese = "#131315",
+  BlackCetacean = "#090f2d",
+
+  PinkRasberry = "#EB5193",
+  PinkLittle = "#EFBBD9",
+  PinkMulbery = "#c84d8e",
+
+  WhitePlatinum = "#E7E7E7",
+  WhiteCoffee = "#e6ded2",
+}
