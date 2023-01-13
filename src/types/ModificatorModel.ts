f9333b3671e7719907aa9e75467574bf3a522e60
Andrey Rukavitsyn <rkvtsn@gmail.com>
2023-01-13 19:17:39 +0100

    modificator model with array of action

--- src/types/ModificatorModel.ts
+++ src/types/ModificatorModel.ts
@@ -3,19 +3,19 @@ import { ICharacterService } from "services/CharacterService";
 export interface ModificatorModel {
   name: string;
   icon?: string;
-  action: string;
+  action: string[];
   apply(character: ICharacterService): void;
 }
 
 export class ModificatorForestNatureMove implements ModificatorModel {
   name: string;
   icon?: string | undefined;
-  action: string;
+  action: string[];
   apply(character: ICharacterService): void {
     // if (this.action !== action) return false;
   }
 
-  constructor(name: string, action: string, icon?: string) {
+  constructor(name: string, action: string[], icon?: string) {
     this.name = name;
     this.icon = icon;
     this.action = action;
